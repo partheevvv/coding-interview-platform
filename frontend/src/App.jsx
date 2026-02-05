44368846e2accde305d3536e2a03956e3b043f28
partheevvv <parthivsaha8436@gmail.com>
2026-02-05 23:19:41 +0530

    HomePage Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import HomePage from './pages/HomePage'
-import { Navigate, Route, Routes } from 'react-router'
-import { useUser } from '@clerk/clerk-react'
-import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router'
 
 function App() {
-
-  const {isSignedIn} = useUser()
 
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/problems" element={isSignedIn? <ProblemsPage />: <Navigate to={"/"} />} />
       </Routes>
-      <Toaster toastOptions={{ duration:3000 }}/> 
     </>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
-import toast from "react-hot-toast"
+import {Link} from "react-router";
+import {Binary} from "lucide-react";
+
 
 function HomePage() {
   return (
-    <button className="btn btn-secondary" onClick={() => toast.success("This is a success toast")}>Click Me</button>
+    <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
+      {/*NAVBAR*/}
+      <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+        <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
+          {/*LOGO*/}
+          <Link to={"/"} className="flex items-center gap-3 hover:scale-105 transition-transform duration-200">
+            <div className="size-10 rounded-xl bg-gradient-to-br from-primary via-secondary to-accent flex items-center justify-center shadow-lg">
+              <Binary className="size-6 text-white" />
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </div>
     )
 }
 
